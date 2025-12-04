3e7518ed698a8967e20e0027dfbbe38e11dd1063
Kallimar <deepakkallimar10@gmail.com>
2025-12-05 01:15:18 +0530

    Full app done, there could be a problem in injset functions for the calculateNextRecurringDate, but i think its fixed

--- actions/dashboard.js
+++ actions/dashboard.js
@@ -31,7 +31,6 @@ export async function createAccount(data) {
     if (!user) {
       throw new Error("User not found");
     }
-    
 
     //convert balnce to float before saving
     const balanceFloat = parseFloat(data.balance);
@@ -105,4 +104,25 @@ export async function getUserAccounts() {
   const serializedAccount = accounts.map(serializeTransaction);
 
   return serializedAccount;
+}
+
+export async function getDashboardData() {
+  const { userId } = await auth();
+  if (!userId) throw new Error("Unauthorized");
+
+  const user = await db.user.findUnique({
+    where: { clerkUserId: userId },
+  });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
+  // Get all user transactions
+  const transactions = await db.transaction.findMany({
+    where: { userId: user.id },
+    orderBy: { date: "desc" },
+  });
+
+  return transactions.map(serializeTransaction);
 }
--- app/(main)/dashboard/page.jsx
+++ app/(main)/dashboard/page.jsx
@@ -1,12 +1,12 @@
-import { getUserAccounts } from "@/actions/dashboard";
+import { getDashboardData, getUserAccounts } from "@/actions/dashboard";
 import CreateAccountDrawer from "@/components/create-account-drawer";
 import { Card, CardContent } from "@/components/ui/card";
 import { Plus } from "lucide-react";
-import React from "react";
+import React, { Suspense } from "react";
 import AccountCard from "./_components/account-card";
 import { getCurrentBudget } from "@/actions/budget";
 import { BudgetProgress } from "./_components/budget-progress";
-
+import DashboardOverview from "./_components/transaction-overview";
 
 async function DashboardPage() {
   const accounts = await getUserAccounts();
@@ -17,6 +17,7 @@ async function DashboardPage() {
   if (defaultAccount) {
     budgetData = await getCurrentBudget(defaultAccount.id);
   }
+  const transactions = await getDashboardData();
 
   return (
     <div className="space-y-8">
@@ -29,6 +30,12 @@ async function DashboardPage() {
       )}
 
       {/* overview */}
+      <Suspense fallback={"Loading Overview..."}>
+        <DashboardOverview
+          accounts={accounts}
+          transactions={transactions || []}
+        />
+      </Suspense>
 
       {/* Accounts grid */}
 
